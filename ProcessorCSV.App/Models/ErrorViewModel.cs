1b0fe3c0f3ec22fb8037d61286399b09c9ecc1a3
sspeelman <sspeelman@thecodegroup.co.za>
2021-04-10 17:08:08 +0200

    initial

---
+++ ProcessorCSV.App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProcessorCSV.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
