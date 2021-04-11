4a913faa4fe766164c3d8619170c142884414d8d
sspeelman <sspeelman@thecodegroup.co.za>
2021-04-11 15:19:31 +0200

    added datatable util

---
+++ ProcessorCSV.Util/Models/DataTableParameters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProcessorCSV.Util.Models
+{
+    /// <summary>
+    /// <c>DataTableParams</c> class to handle DataTable Events
+    /// </summary>
+    /// <remarks>
+    /// Data being POST from DataTableJS(new versions)
+    /// </remarks>
+    public class DataTableParams
+    {
+        public int draw { get; set; } = 1;
+        public int start { get; set; }
+        public int length { get; set; }
+        public List<DtOrder> order { get; set; }
+        public DtSearch search { get; set; }
+        public List<DtColumn> columns { get; set; }
+    }
+
+    public class DtOrder
+    {
+        public int column { get; set; }
+        public string dir { get; set; }
+    }
+
+    public class DtColumn
+    {
+        public string data { get; set; }
+        public string name { get; set; }
+        public bool searchable { get; set; }
+        public bool orderable { get; set; }
+        public DtSearch search { get; set; }
+    }
+
+
+    public class DtSearch
+    {
+        public string value { get; set; }
+        public bool regex { get; set; }
+    }
+}
