1e2c97f69738065f30cb30c9fbcc5d76fccd300f
sspeelman <sspeelman@thecodegroup.co.za>
2021-04-11 15:08:01 +0200

    added context and entities

---
+++ ProcessorCSV.DB/dbo/Tables/DocumentItems.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[DocumentItems] (
+    [Id]                UNIQUEIDENTIFIER CONSTRAINT [DF_DocumentItems_Id] DEFAULT (newid()) ROWGUIDCOL NOT NULL,
+    [DocumentID]        UNIQUEIDENTIFIER NOT NULL,
+    [StudentNumber]     NVARCHAR (250)   NOT NULL,
+    [FirstName]         NVARCHAR (250)   NOT NULL,
+    [Surname]           NVARCHAR (250)   NOT NULL,
+    [CourseCode]        NVARCHAR (250)   NOT NULL,
+    [CourseDescription] NVARCHAR (250)   NOT NULL,
+    [Grade]             NVARCHAR (250)   NOT NULL,
+    CONSTRAINT [PK_DocumentItems] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_DocumentItems_Document] FOREIGN KEY ([DocumentID]) REFERENCES [dbo].[Document] ([Id])
+);
+
